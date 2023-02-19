1af64c9ee8fdaa548ff0f2966ec5d80bdba989be
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-02-18 21:53:24 -0500

    FIXED problem with db

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectToDatabase = async () => {
 
-    mongoose.connect('mongodb://localhost:27017/myapp', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }, (err) => {
+    mongoose.connect('mongodb://localhost:27017/myapp',(err) => {
         if (err) {
             console.error('MongoDB connection error:', err);
         } else {
